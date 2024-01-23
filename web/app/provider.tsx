58067fedb0c0994e9cdfbb2385dfeda79fb86fbe
Dmitry Sky <dmitry.sky@icloud.com>
2024-01-23 14:21:52 -0700

    Not using these imports, these probably only come into play when you setup with anchor

--- web/app/provider.tsx
+++ web/app/provider.tsx
@@ -4,6 +4,7 @@ import React, { ReactNode, useState } from 'react';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 export function Provider({ children }: { children: ReactNode }) {
+  // ensure that the client is instantiated once and retained across re-renders
   const [client] = useState(new QueryClient());
 
   return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
