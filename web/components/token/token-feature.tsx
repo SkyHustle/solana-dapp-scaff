56fe644f2d1c560abfc8d0a31b7261643e0d543b
Dmitry Sky <dmitry.sky@icloud.com>
2024-01-27 16:19:16 -0700

    Rafactor, add a mint token button and modal to each Token Account

--- web/components/token/token-feature.tsx
+++ web/components/token/token-feature.tsx
@@ -1,14 +1,12 @@
 'use client';
-import { PublicKey } from '@solana/web3.js';
-import { useState } from 'react';
 import { useWallet } from '@solana/wallet-adapter-react';
-import { AppModal } from '../ui/ui-layout';
 import { WalletButton } from '../solana/solana-provider';
 import { useCreateMint } from './token-data-access';
+import { AccountTokens } from '../account/account-ui';
 
 export default function TokenAccounts() {
   const { publicKey } = useWallet();
-  const [showSendModal, setShowSendModal] = useState(false);
+
   const mutation = useCreateMint({ address: publicKey });
 
   if (!publicKey) {
@@ -27,76 +25,22 @@ export default function TokenAccounts() {
 
   return (
     <>
-      <ModalTokenMint
-        address={publicKey}
-        show={showSendModal}
-        hide={() => setShowSendModal(false)}
-      />
       <div className="space-y-2 pt-10">
         <div className="justify-between">
           <div className="flex justify-between">
-            <h2 className="text-2xl font-bold">Token Mint Address</h2>
-            <div className="space-x-2">
+            <h2 className="text-2xl font-bold">
               <button
                 className="btn btn-xs lg:btn-md btn-outline"
                 onClick={() => createMint()}
               >
                 Create New Token
               </button>
-            </div>
+            </h2>
           </div>
         </div>
-        <div>
-          No Tokens Created Yet
-          <button
-            className="btn btn-xs lg:btn-md btn-outline"
-            onClick={() => setShowSendModal(true)}
-          >
-            Mint Tokens
-          </button>
-        </div>
+        <div></div>
       </div>
+      <AccountTokens address={publicKey} />
     </>
-  );
-}
-
-function ModalTokenMint({
-  hide,
-  show,
-  address,
-}: {
-  hide: () => void;
-  show: boolean;
-  address: PublicKey;
-}) {
-  const wallet = useWallet();
-  const [amount, setAmount] = useState('1');
-
-  if (!address || !wallet.sendTransaction) {
-    return <div>Wallet not connected</div>;
-  }
-
-  return (
-    <AppModal
-      hide={hide}
-      show={show}
-      title="Create Token"
-      submitDisabled={false}
-      submitLabel="Create Token"
-      submit={() => {
-        console.log('Submiting');
-      }}
-    >
-      <input
-        disabled={false}
-        type="number"
-        step="any"
-        min="1"
-        placeholder="Amount"
-        className="input input-bordered w-full"
-        value={amount}
-        onChange={(e) => setAmount(e.target.value)}
-      />
-    </AppModal>
   );
 }
